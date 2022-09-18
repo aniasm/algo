6f42db61f0bc3b99bcbe2573d20b951a48ecd142
aniasm <113475535+aniasm@users.noreply.github.com>
2022-09-18 15:49:21 +0200

    Zad1
    Napisz skrypt w bashu, który wczyta jedną liczbę całkowitą dodatnią i wypisze wszystkie liczby pierwsze mniejsze lub równe liczbie z wejścia.

---
+++ p.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+isPrime() {
+  local n=$1
+  #echo "Param: $n"
+  for((i=2; i<=$n/2; i++))
+  do
+    ans=$(( n%i ))
+    if [ $ans -eq 0 ]
+    then
+      #echo "$n nie pierwsza."
+      return
+    fi
+  done
+  echo "$n "
+  #echo "$n pierwsza."
+}
+
+
+echo -e "Enter Number: \c"
+read num
+result_all='';
+for((j=2; j<=$num; j++))
+do
+  result_all+=$(isPrime $j)
+done
+
+echo "Wyniki: $result_all"
